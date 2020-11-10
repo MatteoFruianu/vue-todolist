e78c4293b538f68d24bb388c2a76e1344ca30fd3
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-10 17:53:33 +0100

    Empty list message

--- javascript/main.js
+++ javascript/main.js
@@ -4,45 +4,44 @@
 
 const app = new Vue ({
 
-el: "#app",
-
-data: {
-
-  // creo array nei data che rappresenta l'elenco di elementi della lista
-  todos: [],
-
-   todo: '', //inizializzo item dell'array come stringa vuota
-
+  el: "#app",
   
-   
+  data: {
   
-},
-
-methods: {
-
-  addTodo() { 
-    this.todos.push(this.todo) //funzione che mi permette di pushare nell'array
-    this.todo = '' //finisce con la stringa vuoto altrimenti il testo inserito rimane nell'input
+    // creo array nei data che rappresenta l'elenco di elementi della lista
+    todos: [],
+  
+     todo: '', //inizializzo item dell'array come stringa vuota
+  
+    
+     
+    
   },
-
-  deleteTodo(index) {
-
-    this.todos.splice(index)
   
-   },
-
-   defaultMessage() {
-     if (this.todos.length === 0) {
-       console.log("ciao");
+  methods: {
+  
+    addTodo() { 
+      this.todos.push(this.todo) //funzione che mi permette di pushare nell'array
+      this.todo = '' //finisce con la stringa vuoto altrimenti il testo inserito rimane nell'input
+    },
+  
+    deleteTodo(index) {
+  
+      this.todos.splice(index)
+    
+     },
+  
+     defaultMessage() {
+       if (this.todos.length === 0) {
+         console.log("ciao");
+       }
      }
-   }
-
+  
+     
+  
+  },
+  
    
-
-},
-
- 
-
-
-})
-
+  
+  
+  })
