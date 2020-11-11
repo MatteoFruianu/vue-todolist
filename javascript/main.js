d07f3992d7588ee8b6e2e3ad32c47d5adf488a9c
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-11 11:46:11 +0100

    deleteall

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,4 @@
-// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
-// BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+
 
 
 const app = new Vue ({
@@ -21,21 +20,26 @@ const app = new Vue ({
   methods: {
   
     addTodo() { 
-      this.todos.push(this.todo) //funzione che mi permette di pushare nell'array
-      this.todo = '' //finisce con la stringa vuoto altrimenti il testo inserito rimane nell'input
+
+      if (this.todo.trim() !== '') {//validazione
+      this.todos.push(this.todo); } //funzione che mi permette di pushare nell'array
+      this.todo = '' //finisce con la stringa vuota altrimenti il testo inserito rimane nell'input
     },
   
     deleteTodo(index) {
   
-      this.todos.splice(index)
+      this.todos.splice(index, 1)
     
      },
-  
-     defaultMessage() {
-       if (this.todos.length === 0) {
-         console.log("ciao");
-       }
+
+     deleteAll(index) {
+       alert("Sei sicuro di voler cancellare tutto?");
+       this.todos.splice(index, this.todos.length);
      }
+  
+    
+
+    
   
      
   
